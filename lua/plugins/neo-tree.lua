c6cb4c94f55fc8420eb20ba6b7d52463f756e792
Mishieck <mishieckmwale@gmail.com>
2024-02-29 17:36:45 +0200

    feat(plugins/neo-tree): show hidden files except the '.git' directory

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,17 @@ return {
     'MunifTanjim/nui.nvim',
   },
   config = function()
-     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {});
+
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,  -- Show hidden files
+          hide_dotfiles = false,  -- Render hidden files as 'dimmed out'
+          hide_gitignored = true,  -- Hide files ignored by Git
+          hide_by_name = { '.git' }  -- Exclude listed directories
+        },
+      },
+    }
   end
 }
